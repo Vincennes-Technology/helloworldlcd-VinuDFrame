9a4069a2e90d13644fd58cbd9375e105228404dd
VinuDFrame <dframe@vinu.edu>
2018-03-19 16:08:36 +0000

    Added if then

--- helloLCD.py
+++ helloLCD.py
@@ -3,10 +3,21 @@
 # DFrame
 
 import Adafruit_CharLCD as LCD
+import time
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
-displayText = "Hello World"
+displayText = "Hello World\n"
 
 lcd.clear()
 lcd.message(displayText)
+
+while (True):
+    if lcd.is_pressed(LCD.UP):
+        lcd.set_backlight(1)
+        lcd.message(displayText)
+
+    else:
+        lcd.set_backlight(0)
+        lcd.clear()
+        time.sleep(0.5)
